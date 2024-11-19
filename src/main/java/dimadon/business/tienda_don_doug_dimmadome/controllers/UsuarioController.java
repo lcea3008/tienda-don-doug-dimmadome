a66246d85ea63fab5dc3fd10eb0e9a6842e55d89
LCA3008 <pec1radragon@gmail.com>
2024-11-19 18:08:16 -0500

    prueba2

--- src/main/java/dimadon/business/tienda_don_doug_dimmadome/controllers/UsuarioController.java
+++ src/main/java/dimadon/business/tienda_don_doug_dimmadome/controllers/UsuarioController.java
@@ -1,13 +1,13 @@
 package dimadon.business.tienda_don_doug_dimmadome.controllers;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import dimadon.business.tienda_don_doug_dimmadome.entities.Usuario;
@@ -30,57 +30,57 @@ public class UsuarioController {
         return this.serviceUsuario.guardarUsuario(usuario);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, String> loginData) {
-        String email = loginData.get("email");
-        String contrasena = loginData.get("contrasena");
+    // @PostMapping("/login")
+    // public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, String> loginData) {
+    //     String email = loginData.get("email");
+    //     String contrasena = loginData.get("contrasena");
 
-        Map<String, Object> response = new HashMap<>();
-        try {
-            Usuario usuario = serviceUsuario.verificarCredenciales(email, contrasena);
-            if (usuario != null) {
-                response.put("success", true);
-                response.put("message", "Login exitoso");
-                response.put("tipoUsuarioId", usuario.getTipoUsuario().getIdTipoUsuario()); // Solo el ID del
-                                                                                            // tipoUsuario
-                return ResponseEntity.ok(response);
-            } else {
-                response.put("success", false);
-                response.put("message", "Credenciales incorrectas");
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-            }
-        } catch (Exception e) {
-            response.put("success", false);
-            response.put("message", "Error en el servidor. Por favor intenta más tarde.");
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-        }
-    }
+    //     Map<String, Object> response = new HashMap<>();
+    //     try {
+    //         Usuario usuario = serviceUsuario.verificarCredenciales(email, contrasena);
+    //         if (usuario != null) {
+    //             response.put("success", true);
+    //             response.put("message", "Login exitoso");
+    //             response.put("tipoUsuarioId", usuario.getTipoUsuario().getIdTipoUsuario()); // Solo el ID del
+    //                                                                                         // tipoUsuario
+    //             return ResponseEntity.ok(response);
+    //         } else {
+    //             response.put("success", false);
+    //             response.put("message", "Credenciales incorrectas");
+    //             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    //         }
+    //     } catch (Exception e) {
+    //         response.put("success", false);
+    //         response.put("message", "Error en el servidor. Por favor intenta más tarde.");
+    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    //     }
+    // }
 
-    // cambiar estado del usuario
-    @PutMapping("/{id}/estado")
-    public ResponseEntity<Usuario> cambiarEstado(@PathVariable("id") int id,
-            @RequestBody Map<String, String> estadoData) {
-        String nuevoEstado = estadoData.get("estado");
-        Usuario usuarioActualizado = serviceUsuario.cambiarEstado(id, nuevoEstado);
-        return new ResponseEntity<>(usuarioActualizado, HttpStatus.OK);
-    }
+    // // cambiar estado del usuario
+    // @PutMapping("/{id}/estado")
+    // public ResponseEntity<Usuario> cambiarEstado(@PathVariable("id") int id,
+    //         @RequestBody Map<String, String> estadoData) {
+    //     String nuevoEstado = estadoData.get("estado");
+    //     Usuario usuarioActualizado = serviceUsuario.cambiarEstado(id, nuevoEstado);
+    //     return new ResponseEntity<>(usuarioActualizado, HttpStatus.OK);
+    // }
 
-    // actualiza segun el id
-    @PutMapping("/{id}")
-    public ResponseEntity<Usuario> actualizarUsuario(@PathVariable("id") int id, @RequestBody Usuario usuario) {
-        Usuario usuarioActualizado = serviceUsuario.actualizarUsuario(id, usuario);
-        if (usuarioActualizado != null) {
-            return ResponseEntity.ok(usuarioActualizado);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
+    // // actualiza segun el id
+    // @PutMapping("/{id}")
+    // public ResponseEntity<Usuario> actualizarUsuario(@PathVariable("id") int id, @RequestBody Usuario usuario) {
+    //     Usuario usuarioActualizado = serviceUsuario.actualizarUsuario(id, usuario);
+    //     if (usuarioActualizado != null) {
+    //         return ResponseEntity.ok(usuarioActualizado);
+    //     } else {
+    //         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    //     }
+    // }
 
-    // obtener usuarios activos e inactivos
-    @GetMapping("/estado/{estado}")
-    public ResponseEntity<List<Usuario>> obtenerUsuariosPorEstado(@PathVariable String estado) {
-        List<Usuario> usuarios = serviceUsuario.obtenerUsuariosPorEstado(estado);
-        return ResponseEntity.ok(usuarios);
-    }
+    // // obtener usuarios activos e inactivos
+    // @GetMapping("/estado/{estado}")
+    // public ResponseEntity<List<Usuario>> obtenerUsuariosPorEstado(@PathVariable String estado) {
+    //     List<Usuario> usuarios = serviceUsuario.obtenerUsuariosPorEstado(estado);
+    //     return ResponseEntity.ok(usuarios);
+    // }
 
 }
--- src/main/java/dimadon/business/tienda_don_doug_dimmadome/services/ServiceUsuario.java
+++ src/main/java/dimadon/business/tienda_don_doug_dimmadome/services/ServiceUsuario.java
@@ -1,8 +1,8 @@
 package dimadon.business.tienda_don_doug_dimmadome.services;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,54 +25,52 @@ public class ServiceUsuario {
     }
 
     public Usuario guardarUsuario(Usuario usuario) {
-        String contrasenaEncriptada = passwordEncoder.encode(usuario.getContrasena());
+        String contrasenaEncripteda = passwordEncoder.encode(usuario.getContrasena());
+        usuario.setContrasena(contrasenaEncripteda);
         return repositoryUsuario.save(usuario);
     }
 
     // login
     public Usuario verificarCredenciales(String email, String contrasena) {
-        Optional<Usuario> usuarioOpt = repositoryUsuario.findByEmail(email);
-        if (usuarioOpt.isPresent()) {
-            Usuario usuario = usuarioOpt.get();
-            if (usuario.getContrasena().equals(contrasena)) {
-                return usuario;
-            }
+        Usuario usuario = repositoryUsuario.findByEmail(email).orElse(null);
+        if (usuario != null && passwordEncoder.matches(contrasena, usuario.getContrasena())) {
+            return usuario;
         }
         return null;
     }
 
-    // cambiar estado del usuario
-    public Usuario cambiarEstado(int id, String nuevoEstado) {
-        Optional<Usuario> usuarioOpt = repositoryUsuario.findById(id);
-        if (usuarioOpt.isPresent()) {
-            Usuario usuario = usuarioOpt.get();
-            usuario.setEstado(nuevoEstado);
-            return repositoryUsuario.save(usuario);
-        } else {
-            throw new RuntimeException("Usuario no encontrado");
-        }
-    }
+    // // cambiar estado del usuario
+    // public Usuario cambiarEstado(int id, String nuevoEstado) {
+    // Optional<Usuario> usuarioOpt = repositoryUsuario.findById(id);
+    // if (usuarioOpt.isPresent()) {
+    // Usuario usuario = usuarioOpt.get();
+    // usuario.setEstado(nuevoEstado);
+    // return repositoryUsuario.save(usuario);
+    // } else {
+    // throw new RuntimeException("Usuario no encontrado");
+    // }
+    // }
 
-    // actualizar usuario por id
-    public Usuario actualizarUsuario(int id, Usuario usuarioActualizado) {
-        Optional<Usuario> usuarioExistenteOpt = repositoryUsuario.findById(id);
-        if (usuarioExistenteOpt.isPresent()) {
-            Usuario usuarioExistente = usuarioExistenteOpt.get();
+    // // actualizar usuario por id
+    // public Usuario actualizarUsuario(int id, Usuario usuarioActualizado) {
+    // Optional<Usuario> usuarioExistenteOpt = repositoryUsuario.findById(id);
+    // if (usuarioExistenteOpt.isPresent()) {
+    // Usuario usuarioExistente = usuarioExistenteOpt.get();
 
-            usuarioExistente.setNombre(usuarioActualizado.getNombre());
-            usuarioExistente.setEmail(usuarioActualizado.getEmail());
-            usuarioExistente.setContrasena(usuarioActualizado.getContrasena());
-            usuarioExistente.setTipoUsuario(usuarioActualizado.getTipoUsuario());
-            usuarioExistente.setEstado(usuarioActualizado.getEstado());
+    // usuarioExistente.setNombre(usuarioActualizado.getNombre());
+    // usuarioExistente.setEmail(usuarioActualizado.getEmail());
+    // usuarioExistente.setContrasena(usuarioActualizado.getContrasena());
+    // usuarioExistente.setTipoUsuario(usuarioActualizado.getTipoUsuario());
+    // usuarioExistente.setEstado(usuarioActualizado.getEstado());
 
-            return repositoryUsuario.save(usuarioExistente);
-        } else {
-            return null;
-        }
-    }
+    // return repositoryUsuario.save(usuarioExistente);
+    // } else {
+    // return null;
+    // }
+    // }
 
-    // obtener usuarios activos e inactivos
-    public List<Usuario> obtenerUsuariosPorEstado(String estado) {
-        return repositoryUsuario.findByEstado(estado);
-    }
+    // // obtener usuarios activos e inactivos
+    // public List<Usuario> obtenerUsuariosPorEstado(String estado) {
+    // return repositoryUsuario.findByEstado(estado);
+    // }
 }
